a2c840664628c0ba6d7aa578857ead25aa12cf5f
Andreas Völker <andreas.volker@shiftinc.com.br>
2018-01-11 15:48:52 -0200

    nuts

--- src/app/route/route.ts
+++ src/app/route/route.ts
@@ -6,17 +6,62 @@ import { Utils } from './../utils/utils';
 const today: string = Utils.date2str('');
 const tomorrow: string = Utils.date2str('', new Date((new Date()).getTime() + (864e5)))
 
-export const Route:Routes = [{
+export const Route: Routes = [
+{
     path: '',
-    children: [{
-        path: '',
-        component: RootComponent
-    }, {
-        path: 'detalhe',
-        children: [{
+    children: [
+        {
             path: '',
-            redirectTo: '/',
-            pathMatch: 'full'
+            component: RootComponent
+        }, {
+            path: 'detalhe',
+            children: [{
+                path: '',
+                redirectTo: '/',
+                pathMatch: 'full'
+            }, {
+                path: ':id',
+                children: [{
+                    path: ':in',
+                    children: [{
+                        path: ':out',
+                        children: [{
+                            path: ':rooms',
+                            children: [{
+                                path: '',
+                                component: DetalheComponent,
+                                pathMatch: 'full',
+                                children: [{
+                                    path: '**',
+                                    redirectTo: 'room1',
+                                    pathMatch: 'full'
+                                }]
+                            }, {
+                                path: '**',
+                                redirectTo: 'test',
+                                pathMatch: 'full'
+                            }]
+                        }, {
+                            path: '**',
+                            redirectTo: 'room1',
+                            pathMatch: 'full'
+                        }]
+                    }]
+                }, {
+                    path: '**',
+                    redirectTo: today + '/' + today + '/' + 'room1',
+                    pathMatch: 'full'
+                }]
+            }]
+        }, {
+            path: 'booking',
+            component: RootComponent
+        }, {
+            path: 'confirmacao',
+            component: RootComponent
+        }, {
+            path: 'home',
+            redirectTo: '/'
         }, {
             path: ':id',
             children: [{
@@ -27,7 +72,7 @@ export const Route:Routes = [{
                         path: ':rooms',
                         children: [{
                             path: '',
-                            component: DetalheComponent,
+                            component: RootComponent,
                             pathMatch: 'full',
                             children: [{
                                 path: '**',
@@ -36,7 +81,7 @@ export const Route:Routes = [{
                             }]
                         }, {
                             path: '**',
-                            redirectTo: 'test',
+                            redirectTo: 'testa',
                             pathMatch: 'full'
                         }]
                     }, {
@@ -44,61 +89,21 @@ export const Route:Routes = [{
                         redirectTo: 'room1',
                         pathMatch: 'full'
                     }]
-                }]
-            }, {
-                path: '**',
-                redirectTo: today + '/' + today + '/' + 'room1',
-                pathMatch: 'full'
-            }]
-        }]
-    }, {
-        path: 'booking',
-        component: RootComponent
-    }, {
-        path: 'confirmacao',
-        component: RootComponent
-    }, {
-        path: 'home',
-        redirectTo: '/'
-    }, {
-        path: ':id',
-        children: [{
-            path: ':in',
-            children: [{
-                path: ':out',
-                children: [{
-                    path: ':rooms',
-                    children: [{
-                        path: '',
-                        component: RootComponent,
-                        pathMatch: 'full',
-                        children: [{
-                            path: '**',
-                            redirectTo: 'room1',
-                            pathMatch: 'full'
-                        }]
-                    }, {
-                        path: '**',
-                        redirectTo: 'testa',
-                        pathMatch: 'full'
-                    }]
                 }, {
                     path: '**',
-                    redirectTo: 'room1',
+                    redirectTo: today + '/' + 'room1',
                     pathMatch: 'full'
                 }]
             }, {
                 path: '**',
-                redirectTo: today + '/' + 'room1',
+                redirectTo: '/',
                 pathMatch: 'full'
             }]
-        }, {
-            path: '**',
-            redirectTo: '/',
-            pathMatch: 'full'
-        }]
-    }]
-}, {
-    path: '**',
-    component: RootComponent
-}];
+        }
+x    ]
+},
+{
+        path: '**',
+        component: RootComponent
+}
+];

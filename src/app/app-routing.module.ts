b42f39cde50d8099d33d6e705f809ce5114ca15f
Andreas Völker <andreas.volker@shiftinc.com.br>
2018-01-18 16:14:18 -0200

    map/.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,40 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { OptionalRoute } from './app.fn'
+import { RouterModule, UrlMatcher, UrlSegment } from '@angular/router';
 import { RootComponent } from './root/root.component';
 import { BuscaComponent } from './busca/busca.component';
 import { BookingComponent } from './booking/booking.component';
 import { ConfirmacaoComponent } from './confirmacao/confirmacao.component';
 import { DetalheComponent } from './detalhe/detalhe.component';
 
+function OptionalRoute(path: string): UrlMatcher {
+    let parts = path.split('/');
+
+    return (urlSegments: UrlSegment[]) => {
+        let posParams: {
+            [name: string]: UrlSegment;
+        } = {};
+        for (let i = 0; i < parts.length; i++) {
+            if (parts[i].substr(0, 1) == ':' && parts[i].substr(-1, 1) == '?') {
+                if (urlSegments[i]) {
+                    posParams[parts[i].substr(1, parts[i].length - 2)] = urlSegments[i];
+                }
+            } else if (parts[i].substr(0, 1) == ':') {
+                if (!urlSegments[i]) {
+                    return null;
+                }
+                posParams[parts[i].substr(1, parts[i].length - 1)] = urlSegments[i];
+            } else {
+                if (!urlSegments[i] || urlSegments[i].path != parts[i]) {
+                    return null;
+                }
+            }
+        }
+        return {
+            consumed: urlSegments,
+            posParams: posParams
+        };
+    };
+}
 @NgModule({
     imports: [RouterModule.forRoot([{
             path: 'booking',
@@ -17,9 +45,11 @@ import { DetalheComponent } from './detalhe/detalhe.component';
             component: ConfirmacaoComponent
         },
         {
+            matcher: OptionalRoute('detalhe/:id/:in?/:out?/:apt?'),
             component: DetalheComponent
         },
         {
+            matcher: OptionalRoute(':id/:in?/:out?/:apt?/:page?/:sort?/:fName?/:fMask?'),
             component: BuscaComponent
         }
     ])],
